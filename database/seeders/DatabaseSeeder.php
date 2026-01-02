934f85a1e4863ae6d20f507285da51767dc32015
unknown <dwiardi60@gmail.com>
2026-01-02 18:49:23 +0800

    Fix Migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(AdminUserSeeder::class);
         \App\Models\User::factory(30)->create();
 
         // \App\Models\User::factory()->create([
